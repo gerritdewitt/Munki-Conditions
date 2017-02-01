63b49bf71f03ff7e07c389ea38de6782cbeb63ce
gdewitt <gdewitt@gsu.edu>
2017-02-01 14:33:32 -0500

    updates to how we check for ad connectivity - no longer using a sample uid

--- package-root/usr/local/munki/conditions/ad-status.py
+++ package-root/usr/local/munki/conditions/ad-status.py
@@ -9,59 +9,90 @@
 # (ReadMe.md and those in the Documentation folder).
 
 # Written by Gerrit DeWitt (gdewitt@gsu.edu)
-# This file created 2015-08-25 (extensions), 2015-09-11 (initial installcheck scripts for bundle eligibility), 2015-11-10 (extensions), 2015-11-24 (extensions/conditions), 2016-02-16, 2016-06-15, 2016-06-27.
+# This file created 2015-08-25 (extensions), 2015-09-11 (initial installcheck scripts for bundle eligibility)
+# 2015-11-10 (extensions), 2015-11-24 (extensions/conditions), 2016-02-16, 2016-06-15, 2016-06-27
+# 2017-01-13.
 # Copyright Georgia State University.
 # This script uses publicly-documented methods known to those skilled in the art.
 
 # Variables:
 global AD_FOREST, AD_DOMAIN
-# Edit to meet your needs:
-# If domain is forest, these are the same.
-AD_FOREST = "forest.org"
-AD_DOMAIN = "domain.forest.org"
-
-global AD_TEST_USER_NAME, AD_TEST_USER_EXPECTED_UID
-# Edit to meet your needs:
-AD_TEST_USER_NAME = "test-domain-user"
-AD_TEST_USER_EXPECTED_UID = "1234567"
+AD_FOREST = "example.org"
+AD_DOMAIN = "domain.example.org"
 global DEPENDENT_CONFIG_PROFILE_IDENTIFIERS
-# Edit to meet your needs:
 DEPENDENT_CONFIG_PROFILE_IDENTIFIERS = ["org.sample.config.profile.active-directory","org.sample.config.profile.8021X"]
 
-import sys, plistlib, xml, subprocess, os, logging, pwd, time
+import sys, plistlib, xml, subprocess, os, logging, time
 this_dir = os.path.dirname(os.path.realpath(__file__))
 shared_support_dir = os.path.join(this_dir,'shared-support')
 sys.path.append(shared_support_dir)
 import conditions_common
 
-def lookup_test_user(given_name,given_uid):
-    '''Looks up the given user account.  Returns true iff the UID matches the given one.'''
-    measured_uid = -1000
-    try:
-        measured_uid = pwd.getpwnam(given_name).pw_uid
-    except KeyError:
-        pass
-    if int(measured_uid) == int(given_uid):
-        return True
-    else:
-        return False
-
-def dsconfigad_verify_forest_and_domain(given_forest,given_domain):
-    '''Runs dsconfigad -show and inspects its output.
-        Returns true iff the system seems bound to the domain.'''
+def dscl_lookup_computer_record(given_forest,given_domain,given_computer_account):
+    '''Looks up the given computer account using dscl to test communication
+        with the domain.  Returns true iff:
+        1. dscl can communicate with AD, and
+        2. dscl can look up and read attributes of the computer record, and
+        3. the computer record's meta node location contains the
+           given domain or forest.
+        Otherwise, returns false.'''
     # Defaults:
-    output = ''
-    output_dict = {}
+    record_in_domain_or_forest = False
+    # Loop:
+    t = 1
+    while True:
+        if t > 5:
+            break
+        # Call dscl:
+        output_array = []
+        try:
+            output = subprocess.check_output(['/usr/bin/dscl',
+                                              '/Search',
+                                              'read',
+                                              'Computers/%s' % given_computer_account])
+        except subprocess.CalledProcessError:
+            output = ''
+        if output:
+            output_array = output.replace(':\n','\n').replace(': \n','\n').replace(': ','\n').split('\n')
+        if output_array:
+            break
+        # Sleep and try again if no output:
+        time.sleep(10)
+        t += 1
+    # Parse output:
+    try:
+        i = output_array.index('AppleMetaNodeLocation')
+    except ValueError:
+        i = -1
+    if i >= 0:
+        try:
+            if given_forest.lower() in output_array[i+1].lower() or given_domain.lower() in output_array[i+1].lower():
+                record_in_domain_or_forest = True
+        except IndexError:
+            pass
+    # Return:
+    return record_in_domain_or_forest
+
+def dsconfigad_get_computer_record(given_forest,given_domain):
+    '''Runs dsconfigad -show and inspects its output.
+        Returns the computer record name if all these conditions are met:
+        1. macOS thinks the system is bound, and
+        2. the forest and domain match what we expect, and
+        3. dsconfigad returns a computer record name.
+        In all other cases, a blank string is returned.'''
+    # Defaults:
     measured_forest = ''
     measured_domain = ''
+    computer_account = ''
     # Call dsconfigad:
     try:
         output = subprocess.check_output(['/usr/sbin/dsconfigad',
                                           '-xml',
                                           '-show'])
     except subprocess.CalledProcessError:
-        pass
+        output = ''
     # Try to parse output:
+    output_dict = {}
     if output:
         try:
             output_dict = plistlib.readPlistFromString(output)
@@ -76,10 +107,17 @@ def dsconfigad_verify_forest_and_domain(given_forest,given_domain):
             measured_domain = output_dict['General Info']['Active Directory Domain']
         except KeyError:
             pass
+        try:
+            computer_account = output_dict['General Info']['Computer Account']
+        except KeyError:
+            pass
+    # If forest and domain match, and we have a computer record,
+    # return that record name.
     if measured_forest.lower() == given_forest.lower() and measured_domain.lower() == given_domain.lower():
-        return True
-    else:
-        return False
+        if computer_account:
+            return computer_account
+    # Otherwise, return blank string:
+    return ''
 
 def dig_lookup_dns_srv(given_forest,given_domain):
     '''Performs DNS-SRV queries to see if we have domain controllers available.
@@ -87,20 +125,21 @@ def dig_lookup_dns_srv(given_forest,given_domain):
         Returns false otherwise.'''
     # Defaults:
     ad_gc_available = False
-    this_try = 0
-    max_tries = 5
-    # Call dig:
+    # Loop:
+    t = 1
     while True:
-        output = ''
+        if t > 5:
+            break
         output_array = []
         answer_section_index = -1
+        # Call dig:
         try:
             output = subprocess.check_output(['/usr/bin/dig',
                                               '-t',
                                               'SRV',
                                               '_gc._tcp.%s' % given_forest])
         except subprocess.CalledProcessError:
-            pass
+            output = ''
         # Try to parse output:
         if output:
             output_array = output.split('\n')
@@ -111,52 +150,57 @@ def dig_lookup_dns_srv(given_forest,given_domain):
                 pass
         # Break if answer section has our domain:
         if answer_section_index >= 0:
-            try:
-                first_srv_record = output_array[answer_section_index +1]
-                if first_srv_record.find(given_domain) != -1:
+            for i in range(answer_section_index+1,len(output_array)):
+                if given_domain.lower() in output_array[i].lower():
                     ad_gc_available = True
                     break
-            except ValueError:
-                pass
-        # Break if we exceed max tries:
-        if this_try >= max_tries:
-            break
-        # Increment:
-        this_try += 1
+                if given_forest.lower() in output_array[i].lower():
+                    ad_gc_available = True
+                    break
+                if ';' in output_array[i]: # new section; ignore from here
+                    break
+        # Sleep and try again if no output:
+        t += 1
         time.sleep(5)
     # Return:
     return ad_gc_available
 
 def main():
     '''Main logic for this script'''
-    # Default: Assume not on network and all bools False:
+    # Assume not on network unless we prove otherwise.
     ad_status = "not-on-network"
-    ad_dns_srv_found = False
-    ad_test_user_lookup = False
-    ad_test_dsconfigad = False
-    
+
     # Network test:
+    logging.info('Looking for DNS-SRV records...')
     ad_dns_srv_found = dig_lookup_dns_srv(AD_FOREST,AD_DOMAIN)
+    # Basic info from macOS:
+    logging.info('Getting information from dsconfigad...')
+    dsconfigad_computer_record = dsconfigad_get_computer_record(AD_FOREST,AD_DOMAIN)
+    # Connectivity test should be false unless we determine the test is necessary:
+    dscl_lookup_result = False
     
-    # Perform AD tests and evaluate iff on network:
+    # If on network, assume unbound unless we prove otherwise.
     if ad_dns_srv_found:
-        ad_test_user_lookup = lookup_test_user(AD_TEST_USER_NAME,AD_TEST_USER_EXPECTED_UID)
-        ad_test_dsconfigad = dsconfigad_verify_forest_and_domain(AD_FOREST,AD_DOMAIN)
-        if ad_test_user_lookup and ad_test_dsconfigad:
+        logging.info('On network: DNS-SRV records found.')
+        ad_status = "on-network-unbound"
+    # If on network and macOS thinks it is bound, test for connectivity.
+    if ad_dns_srv_found and dsconfigad_computer_record:
+        logging.info('dsconfigad indicates system bound using %s' % dsconfigad_computer_record)
+        dscl_lookup_result = dscl_lookup_computer_record(AD_FOREST,AD_DOMAIN,dsconfigad_computer_record)
+        if dscl_lookup_result:
             ad_status = "on-network-communicating"
-        else:
-            ad_status = "on-network-unbound"
 
     # If unbound, clear out any data in Munki so that profiles
     # for binding may be reinstalled:
     if ad_status == "on-network-unbound":
-        conditions_common.clear_munki_config_profile_data(DEPENDENT_CONFIG_PROFILE_IDENTIFIERS)
+        for profile_identifier in DEPENDENT_CONFIG_PROFILE_IDENTIFIERS:
+            conditions_common.remove_profile(profile_identifier)
 
     # Write Conditions:
     conditions_common.write_conditions({"ad_dns_srv_found":ad_dns_srv_found,
-                     "ad_test_user_lookup":ad_test_user_lookup,
-                     "ad_test_dsconfigad":ad_test_dsconfigad,
-                     "ad_status":ad_status,
+                                       "computer_record":dsconfigad_computer_record,
+                                       "dscl_lookup_result":dscl_lookup_result,
+                                       "ad_status":ad_status,
                      })
     # Finish:
     sys.exit(0)
